5a319f02eb6ff8c6a4d00bd44c61cf667ae3263d
rflorian2017 <roby_florian@yahoo.com>
2020-07-25 16:41:45 +0300

    added registration

---
+++ src/main/java/org/galati2/springtime/controllers/LoginController.java
@@ -0,0 +1,13 @@
+package org.galati2.springtime.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "registration/login";
+    }
+}
